ab1a6616dd5768d4524ba07773b954d2ed21303c
Pablo Toapanta <toapantapablo233@gmail.com>
2025-10-28 18:42:11 -0500

    Actualizacion2

--- src/Parcial2/Cartas1/JuegoArray/Jugador.java
+++ src/Parcial2/Cartas1/JuegoArray/Jugador.java
@@ -1,4 +1,35 @@
 package Parcial2.Cartas1.JuegoArray;
 
 public class Jugador {
+    private final String nombre;
+    private Carta[] mano;
+    private int puntuacion;
+
+    public Jugador(String nombre){
+        this.nombre = nombre;
+        this.puntuacion = 0;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Carta[] getMano() {
+        return mano;
+    }
+
+    public void setMano(Carta[] mano) {
+        this.mano = mano;
+    }
+
+    public int getPuntuacion() {
+        return puntuacion;
+    }
+
+    public void setPuntuacion(int puntuacion) {
+        this.puntuacion = puntuacion;
+    }
+    public void recibirCartas(Carta[] cartas){
+        this.mano = new Carta[cartas.length];
+    }
 }
