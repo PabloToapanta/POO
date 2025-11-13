7de59c5e4d56b4a5d65d7edcbb9daa620f5eac90
Pablo Toapanta <toapantapablo233@gmail.com>
2025-11-13 16:35:19 -0500

    Actualizacion

--- src/Parcial2/MiniProyectos/BatallaMonstruos/Batalla.java
+++ src/Parcial2/MiniProyectos/BatallaMonstruos/Batalla.java
@@ -1,4 +1,35 @@
 package Parcial2.MiniProyectos.BatallaMonstruos;
 
+import java.util.Date;
+
 public class Batalla {
+    private int Ronda=0;
+    private Jugador jugador1,jugador2;
+
+    public Batalla(String nombre1, String nombre2){
+        jugador1= new Jugador(nombre1);
+        jugador2= new Jugador(nombre2);
+    }
+
+    public void jugar(){
+        System.out.println("-----INICIO DEL JUEGO--------");
+
+        while(true){
+
+
+
+
+        }
+
+
+
+
+
+    }
+
+
+
+
+
 }
+
